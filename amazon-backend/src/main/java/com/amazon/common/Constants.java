98783da96ba56e03365aad10f6a0eef25fea6c3c
nallakumarnetha <nallakumarnetha@gmail.com>
2025-11-25 21:25:46 +0530

    save work

--- amazon-backend/src/main/java/com/amazon/common/Constants.java
+++ amazon-backend/src/main/java/com/amazon/common/Constants.java
@@ -9,11 +9,11 @@ public class Constants {
 	
 	
 	// Oauth 2.0 - Authorization Code Flow | Google
-//	public static final String CLIENT_ID_GOOGLE = "853320511518-v0s4mpgoqivhcs8ui04ekmjts4e4qf9v.apps.googleusercontent.com";
-//
-//	public static final String CLIENT_SECRET_GOOGLE = "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";
-//
-//	public static final String PROJECT_ID_GOOGLE = "my-project-01-479011";
+	public static final String CLIENT_ID_GOOGLE = "";
+
+	public static final String CLIENT_SECRET_GOOGLE = "";
+
+	public static final String PROJECT_ID_GOOGLE = "";
 
 	public static final String AUTH_URI_GOOGLE = "https://accounts.google.com/o/oauth2/auth";
 
