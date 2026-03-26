e36f71b188876a58cbedffaef873807f1928e33a
nallakumarnetha <nallakumarnetha@gmail.com>
2026-03-26 08:29:02 +0530

    Added app started log

--- amazon-backend-microservices/common-service/src/main/java/com/common_service/CommonServiceApplication.java
+++ amazon-backend-microservices/common-service/src/main/java/com/common_service/CommonServiceApplication.java
@@ -1,6 +1,7 @@
 package com.common_service;
 
 import org.springframework.boot.SpringApplication;
+import static com.shared_contract.original.Logger.log;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -10,6 +11,7 @@ public class CommonServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommonServiceApplication.class, args);
+		log.info("\u001B[32m✅ Common Service Application started successfully!\u001B[0m");
 	}
 
 }
